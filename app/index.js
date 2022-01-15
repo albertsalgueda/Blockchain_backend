1dae12be3bbd177f4bc85784484d415d0a8835f7
albertsalgueda <albert.salgueda01@estudiant.upf.edu>
2022-01-14 20:53:21 -0500

    Update index.js
    blockchain infrastructure

--- app/index.js
+++ app/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const Blockchain = require('../blockchain');
 // We use process.env.http_port in case that local host 3001 is being used for another task
 // example: on the terminal we would run
@@ -9,11 +10,20 @@ const app = express();
 
 const bc = new Blockchain();
 
+app.use(bodyParser.json());
+
 // first endpoint of our API
 //req -> request || res -> respond
 app.get('/blocks', (req,res)=>{
     res.json(bc.chain);
-})
+});
 
+//data of the block will be sent through the API in a JSON format
+//We'll create a new block with that data
+app.post('/mine',(req,res)=>{
+    const block = bc.addBlock(req.body.data);
+    console.log(`New block added: ${block.toString()}`);
+    res.redirect('/blocks');
+}); 
 
-app.listen(HTTP_PORT, () => console.log('Listening on port ${HTTP_PORT}'));
+app.listen(HTTP_PORT, () => console.log(`Listening on port ${HTTP_PORT}`));
